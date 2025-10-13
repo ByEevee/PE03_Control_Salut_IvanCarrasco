d0144046af6d131f81267c183c5a2ff0de2dbe00
Ivan Carrasco <alu.ivan.carrasco@mataro.epiaedu.cat>
2025-10-13 18:01:11 +0200

    V 3.0 Acabat Introducció de dades

--- Control_Salut.java
+++ Control_Salut.java
@@ -1,7 +1,5 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-@SuppressWarnings("UseOfSystemOutOrErr")
-
 public class Control_Salut {
 public static void main(String[] args) {
     //declarar variables
@@ -11,6 +9,7 @@ public static void main(String[] args) {
     int edat;
     double pes,alçada;
     Scanner esc = new Scanner(System.in);
+    boolean dadesIntroduides = false;
     while (true) //Bucle infinit per tornar al menu principal 
     {
     //inicialitzar menu 
@@ -32,6 +31,7 @@ public static void main(String[] args) {
             case "a", "a)", "A", "A)", "Introduir dades personals", "introduir dades personals", "a) Introduir dades personals", "A) Introduir dades personals", "a) introduir dades personals", "A) introduir dades personals":
                 
                 System.out.println("Has triat l'opcio: Introduir dades personals");
+                System.out.println(" ");
                 System.out.println("Introdueix el teu nom complet:");
                 nomComplet = esc.nextLine();
             
@@ -52,7 +52,7 @@ public static void main(String[] args) {
                 break;
             
                 }
-                else if (!nomComplet.matches("[a-zA-ZÀ-ÿ\\s]"))// si el nom no conté només lletres i espais
+                else if (nomComplet.matches("[a-zA-ZÀ-ÿ\\s]"))// si el nom no conté només lletres i espais
                 {
                 System.out.println("ERROR: El nom no pot contenir números.");
                 System.out.println("");
@@ -60,98 +60,150 @@ public static void main(String[] args) {
                 System.out.println("");
                 break;
                 }
-           
-        
-
-            System.out.println("Introdueix la teva edat:");
-            try {
-            edat = esc.nextInt();
-            esc.nextLine(); // neteja el buffer
-            if (edat <= 0 || edat > 120) {
-                System.out.println("ERROR: L'edat és massa gran o petita.");
-                System.out.println("");
-                System.out.println("Tornant al menú principal...");
-                System.out.println("");
-                break;
-            }
-            }       
-            catch (InputMismatchException e) {
-                System.out.println("Edat no pot ser decimal ni text.");
-                System.out.println("");
-                System.out.println("Tornant al menú principal...");
-                System.out.println("");
-            
-            break; 
-            
-            }
-            catch (Exception e) 
-            {
-                System.out.println("Error: Introdueix un valor vàlid per a l'edat.");
-                System.out.println("");
-                System.out.println("Tornant al menú principal...");
-                System.out.println("");
-                           
-                break;
-        }
-
-
+    
+                try {
                 System.out.println(" ");
-                System.out.println("Introdueix el teu pes (en kg):");
-                pes = esc.nextDouble();
-                if (pes <= 0) 
+                System.out.println("Introdueix la teva edat:");
+                edat = esc.nextInt();
+                    if (edat <= 0 || edat > 120) 
+                    {
+                        System.out.println("ERROR: L'edat és massa gran o petita.");
+                        System.out.println("");
+                        System.out.println("Tornant al menú principal...");
+                        System.out.println("");
+                        break;
+                    }
+                }       
+                catch (InputMismatchException e) 
                 {
-                    System.out.println("ERROR: El pes és massa petit.");
+                    System.out.println("Edat no pot ser decimal ni text.");
                     System.out.println("");
                     System.out.println("Tornant al menú principal...");
                     System.out.println("");
-                    
-                    break;
-                    
-                }
-                else if (pes > 400) 
-                {
-                    System.out.println("ERROR: El pes és massa gran.");
-                    System.out.println("");
-                    System.out.println("Tornant al menú principal...");
-                    System.out.println("");
-                    
-                    break;
-                    
+                
+                    break; 
                 
                 }
-                else if (pes // menys o 2 decimals
-                        != Math.round(pes * 100.0) / 100.0)
+                catch (Exception e) 
                 {
-                    System.out.println("ERROR: El pes és massa petit.");
+                    System.out.println("Error: Introdueix un valor vàlid per a l'edat.");
                     System.out.println("");
                     System.out.println("Tornant al menú principal...");
                     System.out.println("");
-                    
+                            
                     break;
-                    
                 }
-                { 
-                System.out.println(" ");
-                System.out.println("Introdueix la teva alçada (en metres):");
-                alçada = esc.nextDouble();
-                System.out.println(" ");
-                System.out.println("Dades introduides correctament!");
-                System.out.println(" ");
-                System.out.println("Nom complet: " + nomComplet);
-                System.out.println("Edat: "  + " anys");
-                System.out.println("Pes: " + pes + " kg");
-                System.out.println("Alçada: " + alçada + " metres");
+                
 
-                System.out.println("vols tornar al menú principal? (si/no)");
-                tornarAlMenu = esc.next();
-                if (tornarAlMenu.equals("si") || tornarAlMenu.equals("Si") || tornarAlMenu.equals("SI") || tornarAlMenu.equals("sí") || tornarAlMenu.equals("Sí") || tornarAlMenu.equals("SÍ")) {
-                    esc.nextLine(); // consume newline left by next()
-                    break; // Torna al menú principal
-                } else {
-                    System.out.println("Sortint del programa...");
-                    esc.close();
-                    return; // Sortir del programa
+                try {
+                    System.out.println(" ");
+                    System.out.println("Introdueix el teu pes (en kg):");
+                    String entrada = esc.next().replace(',', '.'); 
+                    pes = Double.parseDouble(entrada);
+
+                    // Validar rango
+                    if (pes <= 0 || pes > 400) {
+                        System.out.println("ERROR: El pes ha d'estar entre 0 i 400 kg.");
+                        System.out.println("");
+                        System.out.println("Tornant al menú principal...");
+                        System.out.println("");
+                        break;
+                    }
+
+                    // Validar decimals (máxim 3)
+                    if ((pes *1000) % 1 != 0) {
+                        System.out.println("ERROR: El pes no pot tenir més de 3 decimals.");
+                        System.out.println("");
+                        System.out.println("Tornant al menú principal...");
+                        System.out.println("");
+                        break;
+                    }
+
+
+                } 
+                catch (NumberFormatException e) 
+                {
+                    System.out.println("ERROR: Format de número incorrecte.");
+                    System.out.println("");
+                    System.out.println("Tornant al menú principal...");
+                    System.out.println("");
+                    break;
                 }
+                catch (Exception e) 
+                {
+                    System.out.println("Error: Introdueix un valor vàlid per al pes.");
+                    System.out.println("");
+                    System.out.println("Tornant al menú principal...");
+                    System.out.println("");
+                    break;
+                        
+                }
+                            
+                                
+                try {
+                        System.out.println(" ");
+                        System.out.println("Introdueix la teva alçada (en metres, pots usar coma o punt):");
+
+                        String entrada = esc.next().replace(',', '.'); 
+                        alçada = Double.parseDouble(entrada);
+
+                        if (alçada <= 0.5) 
+                        {
+                            System.out.println("ERROR: L'alçada és massa petita.");
+                            System.out.println("");
+                            System.out.println("Tornant al menú principal...");
+                            System.out.println("");
+                            break;
+                        } 
+                        else if (alçada > 2.5) 
+                        {
+                            System.out.println("ERROR: L'alçada és massa gran, ha d'estar entre 0.5-2.5 metres.");
+                            System.out.println("");
+                            System.out.println("Tornant al menú principal...");
+                            System.out.println("");
+                            break;
+                        }
+
+                        // Validar màxim 3 decimals
+                        
+                        if (alçada * 1000 % 1 != 0)
+                        {
+                            System.out.println("ERROR: L'alçada no pot tenir més de 3 decimals.");
+                            System.out.println("");
+                            System.out.println("Tornant al menú principal...");
+                            System.out.println("");
+                            break;
+                        }
+
+                        
+
+                } catch (Exception e)
+                {
+                    System.out.println("Error: Introdueix un valor vàlid per a l'alçada.");
+                    System.out.println("");
+                    System.out.println("Tornant al menú principal...");
+                    System.out.println("");
+                    break;
+                }
+                
+                    System.out.println(" ");
+                    System.out.println("Dades introduides correctament!");
+                    System.out.println(" ");
+                    dadesIntroduides = true; // Marca que les dades han estat introduïdes
+
+                    
+
+                    System.out.println("vols tornar al menú principal? (si/no)");
+                    tornarAlMenu = esc.next();
+                    if (tornarAlMenu.equals("si") || tornarAlMenu.equals("Si") || tornarAlMenu.equals("SI") || tornarAlMenu.equals("sí") || tornarAlMenu.equals("Sí") || tornarAlMenu.equals("SÍ")) {
+                        esc.nextLine(); 
+                        break; // Torna al menú principal
+                    } else {
+                        System.out.println("Sortint del programa...");
+                        esc.close();
+                        System.exit(0);
+                        ; // Sortir del programa
+                    }
 
           
     
@@ -166,6 +218,6 @@ public static void main(String[] args) {
 
 }
 }
-}
+
 
 
